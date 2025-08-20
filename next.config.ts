6dddb0b563fb4faa2b89a469cd8509c99b4fad2c
rin <strawberinmilk@gmail.com>
2025-08-20 23:29:07 +0900

    gh-pages対応

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/400R-esp-next",
+  assetPrefix: "/400R-esp-next/",
 };
 
 export default nextConfig;
